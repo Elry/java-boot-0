3979f2f7a7d15776993eb6cbabd7265f8e30659d
Elry <rafaelgarcialima@outlook.com>
2021-03-06 22:13:53 -0300

    starting spring and adding code checklist with pmd

---
+++ src/main/java/com/rmt/demo/DemoApplication.java
@@ -0,0 +1,11 @@
+package com.rmt.demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+	public static void main(String[] args) {
+		System.out.println("Goodbye cruel world ...");
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
