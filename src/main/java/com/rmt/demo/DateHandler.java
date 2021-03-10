f181c2a5b54a28e5b6e9febb50f79d9da799ffa7
Elry <rafaelgarcialima@outlook.com>
2021-03-10 19:09:11 -0300

    Very simple date handlers.

---
+++ src/main/java/com/rmt/demo/DateHandler.java
@@ -0,0 +1,27 @@
+package com.rmt.demo;
+
+import java.util.Date;
+import java.util.Calendar;
+import java.time.LocalDate;
+
+public class DateHandler {
+    public Date dateNow(){
+      Date newDT = new Date();
+      return newDT;
+    }
+
+    public Calendar calendarNow(){
+        Calendar cl = Calendar.getInstance();
+        return cl;
+    }
+
+    public LocalDate today(){
+        LocalDate today = LocalDate.now();
+        return today;
+    }
+
+    public LocalDate yesterday(){
+        LocalDate yesterday = today().minusDays(1);
+        return yesterday;
+    }
+}
--- src/main/java/com/rmt/demo/DemoApplication.java
+++ src/main/java/com/rmt/demo/DemoApplication.java
@@ -1,11 +1,18 @@
 package com.rmt.demo;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 	public static void main(String[] args) {
-		System.out.println("Goodbye cruel world ...");
+		DateHandler dh = new DateHandler();
+		
+		System.out.println("Now with date:" + dh.dateNow());
+		System.out.println("Now with calendar: " + dh.calendarNow());
+		System.out.println("Today with local: " + dh.today());
+		System.out.println("Yesterday with local: " + dh.yesterday());
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
