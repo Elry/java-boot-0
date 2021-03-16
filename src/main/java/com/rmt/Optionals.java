58971c7f175191dac0bcb9de5b893ab0f05895b8
Elry <rafaelgarcialima@outlook.com>
2021-03-16 13:28:28 -0300

    Finished all optional ex

---
+++ src/main/java/com/rmt/Optionals.java
@@ -0,0 +1,26 @@
+package com.rmt;
+
+import java.util.Optional;
+import java.util.OptionalInt;
+
+public class Optionals {
+  public static void main(String args[]){
+    Optional<String> opStr = Optional.of("value");
+    opStr.ifPresentOrElse(System.out::println, () -> System.out.println("Not found"));
+
+    Optional<String> nullOpStr = Optional.ofNullable(null);
+    nullOpStr.ifPresentOrElse(System.out::println, () -> System.out.print("Null - not found"));
+
+    Optional<String> emptyOpStr = Optional.empty();
+    emptyOpStr.orElseThrow()
+    emptyOpStr.ifPresentOrElse(System.out::println, () -> System.out.println("Empty - not found"));
+
+    Optional<String> nullErrOpStr = Optional.of(null);
+    nullErrOpStr.ifPresentOrElse(System.out::println, () -> System.out.println("Err - not found"));
+
+    OptionalInt.of(12).ifPresent(System.out::println);
+
+    opStr.map((val) -> val.concat("--")).ifPresent(System.out::println);
+    opStr.orElseThrow(IllegalStateException::new);
+  }
+}
